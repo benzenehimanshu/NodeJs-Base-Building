e6a351a0bb31fc0f4e2973ff69a2e0923ed3f3d5
Himanshu Kumar <benzenehimanshu@gmail.com>
2024-08-27 07:26:24 +0530

    This is database change from local database to online mongodb database

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 //define mongodb connection url
-const mongoURL = "mongodb://localhost:27017/hotels";
+// const mongoURL = "mongodb://localhost:27017/hotels";
+const mongoURL =
+  "mongodb+srv://benzenehimanshu:Bittuyadav123@cluster0.zvhxf.mongodb.net/";
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
